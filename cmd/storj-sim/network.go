20cd2e311c0116515fc1555ecd3daacddc91ee97
Egon Elbre <egonelbre@gmail.com>
2019-02-06 14:47:00 +0200

    Enable ipv6 tests (#1204)
    Change-Id: I1315048b5efdb7216d949bb8943d86d825a6e401

--- cmd/storj-sim/network.go
+++ cmd/storj-sim/network.go
@@ -137,6 +137,9 @@ func newNetwork(flags *Flags) (*Processes, error) {
 		},
 		"run": {},
 	})
+	bootstrap.ExecBefore["run"] = func(process *Process) error {
+		return readConfigString(&bootstrap.Address, bootstrap.Directory, "server.address")
+	}
 
 	// Create satellites making all satellites wait for bootstrap to start
 	var satellites []*Process
@@ -164,10 +167,15 @@ func newNetwork(flags *Flags) (*Processes, error) {
 			},
 			"run": {},
 		})
+
+		process.ExecBefore["run"] = func(process *Process) error {
+			return readConfigString(&process.Address, process.Directory, "server.address")
+		}
 	}
 
 	// Create gateways for each satellite
 	for i, satellite := range satellites {
+		satellite := satellite
 		process := processes.New(Info{
 			Name:       fmt.Sprintf("gateway/%d", i),
 			Executable: "gateway",
@@ -198,6 +206,11 @@ func newNetwork(flags *Flags) (*Processes, error) {
 		})
 
 		process.ExecBefore["run"] = func(process *Process) error {
+			err := readConfigString(&process.Address, process.Directory, "server.address")
+			if err != nil {
+				return err
+			}
+
 			vip := viper.New()
 			vip.AddConfigPath(process.Directory)
 			if err := vip.ReadInConfig(); err != nil {
@@ -241,6 +254,10 @@ func newNetwork(flags *Flags) (*Processes, error) {
 			},
 			"run": {},
 		})
+
+		process.ExecBefore["run"] = func(process *Process) error {
+			return readConfigString(&process.Address, process.Directory, "server.address")
+		}
 	}
 
 	{ // verify that we have all binaries
@@ -300,4 +317,17 @@ func identitySetup(network *Processes) (*Processes, error) {
 	}
 
 	return processes, nil
+}
+
+// readConfigString reads from dir/config.yaml flagName returns the value in `into`
+func readConfigString(into *string, dir, flagName string) error {
+	vip := viper.New()
+	vip.AddConfigPath(dir)
+	if err := vip.ReadInConfig(); err != nil {
+		return err
+	}
+	if v := vip.GetString(flagName); v != "" {
+		*into = v
+	}
+	return nil
 }
